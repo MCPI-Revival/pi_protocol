7c67b151b9a082e623b1fe862dc1122ff6a7cf03
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-04-19 16:08:29 +0300

    Update __init__.py

--- pi_protocol/__init__.py
+++ pi_protocol/__init__.py
@@ -40,6 +40,46 @@ def decode_data_type(data_type: str, stream: object) -> object:
         return stream.read_unsigned_byte()
     if data_type == "Byte":
         return stream.read_byte()
+    if data_type == "UnsignedShortBE":
+        return stream.read_unsigned_short_be()
+    if data_type == "ShortBE":
+        return stream.read_short_be()
+    if data_type == "UnsignedShortLE":
+        return stream.read_unsigned_short_le()
+    if data_type == "ShortLE":
+        return stream.read_short_le()
+    if data_type == "UnsignedTriadBE":
+        return stream.read_unsigned_triad_be()
+    if data_type == "TriadBE":
+        return stream.read_triad_be()
+    if data_type == "UnsignedTriadLE":
+        return stream.read_unsigned_triad_le()
+    if data_type == "TriadLE":
+        return stream.read_triad_le()
+    if data_type == "UnsignedIntBE":
+        return stream.read_unsigned_int_be()
+    if data_type == "IntBE":
+        return stream.read_int_be()
+    if data_type == "UnsignedIntLE":
+        return stream.read_unsigned_int_le()
+    if data_type == "IntLE":
+        return stream.read_int_le()
+    if data_type == "UnsignedLongBE":
+        return stream.read_unsigned_long_be()
+    if data_type == "LongBE":
+        return stream.read_long_be()
+    if data_type == "UnsignedLongLE":
+        return stream.read_unsigned_long_le()
+    if data_type == "LongLE":
+        return stream.read_long_le()
+    if data_type == "FloatBE":
+        return stream.read_float_be()
+    if data_type == "FloatLE":
+        return stream.read_float_le()
+    if data_type == "DoubleBE":
+        return stream.read_double_be()
+    if data_type == "DoubleLE":
+        return stream.read_double_le()
 
 def decode_packet(data: bytes) -> dict:
     pass
